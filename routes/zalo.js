1f8fa58aca4bcc20fc82e3f030c2c55b7ccdf787
Chung Biện <chungch251997@gmail.com>
2020-04-19 15:18:32 +0800

    Test token zalo

--- routes/zalo.js
+++ routes/zalo.js
@@ -8,7 +8,56 @@ const asyncMiddleware = require('../middlewares/async');
 router.get('/', function (req, res, next) {
     if (req.query.event === 'sendmsg') {
         let { message } = req.query.message;
-        replyMessage(req.query.fromuid);
+        let options = null;
+        switch (message.lowercase()) {
+            case 'image': {
+                options = {
+                    method: 'POST',
+                    headers: { 'content-type': 'application/json' },
+                    data: {
+                        "recipient": {
+                            "user_id": "user_id"
+                          },
+                          "message": {
+                            "attachment": {
+                              "payload": {
+                                "elements": [
+                                  {
+                                    "media_type": "image",
+                                    "title": "kaisa",
+                                    "url": "https://4.bp.blogspot.com/-NuSmmm8DNbQ/Xhim8bRWwtI/AAAAAAAATOc/-xusgk4771YEsrdB-6QAZvw-GqjK2mbKgCLcBGAsYHQ/w914-h514-p-k-no-nu/kaisa-lol-season-2020-uhdpaper.com-4K-5.1825-wp.thumbnail.jpg"
+                                  }
+                                ],
+                                "template_type": "media"
+                              },
+                              "type": "template"
+                            },
+                            "text": "hello, world!"
+                          }
+                    },
+                    url:
+                        'https://openapi.zalo.me/v2.0/oa/message?access_token=hQXVTZR9a56ffrbqCisJLA2WMt4xrl4ftj5MS1tGxmJyta9TCl7lETpsMcKnnkiAoyn1RXVByXJ7vMLbFOdJG_MrRsCtrAGunRGHOGsXeX_Md6n4AB3O8zAdI4SlqkLc_zvAFo79_KJVf3eB9QhyUUMyJoiOfuGF-Qe3U0USk5YKhd8WIxx-MggCQn1YjVyo_8DZJ3wjps6db7O0IBZxGRMlKm0diFbj-OPyJpoHy0BylLP6CQhc1ws8NIL5kynSdRCA7d6RX7gfX4irRMAgMFyLDDwJLW',
+                };
+                break;
+            }
+            default:
+                options = {
+                    method: 'POST',
+                    headers: { 'content-type': 'application/json' },
+                    data: {
+                        recipient: {
+                            user_id: uid,
+                        },
+                        message: {
+                            text: '안녕하세요 [an-nhong-ha-sê-yô]',
+                        },
+                    },
+                    url:
+                        'https://openapi.zalo.me/v2.0/oa/message?access_token=hQXVTZR9a56ffrbqCisJLA2WMt4xrl4ftj5MS1tGxmJyta9TCl7lETpsMcKnnkiAoyn1RXVByXJ7vMLbFOdJG_MrRsCtrAGunRGHOGsXeX_Md6n4AB3O8zAdI4SlqkLc_zvAFo79_KJVf3eB9QhyUUMyJoiOfuGF-Qe3U0USk5YKhd8WIxx-MggCQn1YjVyo_8DZJ3wjps6db7O0IBZxGRMlKm0diFbj-OPyJpoHy0BylLP6CQhc1ws8NIL5kynSdRCA7d6RX7gfX4irRMAgMFyLDDwJLW',
+                };
+                break;
+        }
+        replyMessage(req.query.fromuid, options);
     }
     res.sendStatus(200);
 });
@@ -18,22 +67,7 @@ router.post('/', function (req, res, next) {
     res.sendStatus(200);
 });
 
-function replyMessage(uid) {
-    console.log(uid);
-    const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        data: {
-            recipient: {
-                user_id: uid,
-            },
-            message: {
-                text: '안녕하세요 [an-nhong-ha-sê-yô]',
-            },
-        },
-        url:
-            'https://openapi.zalo.me/v2.0/oa/message?access_token=hQXVTZR9a56ffrbqCisJLA2WMt4xrl4ftj5MS1tGxmJyta9TCl7lETpsMcKnnkiAoyn1RXVByXJ7vMLbFOdJG_MrRsCtrAGunRGHOGsXeX_Md6n4AB3O8zAdI4SlqkLc_zvAFo79_KJVf3eB9QhyUUMyJoiOfuGF-Qe3U0USk5YKhd8WIxx-MggCQn1YjVyo_8DZJ3wjps6db7O0IBZxGRMlKm0diFbj-OPyJpoHy0BylLP6CQhc1ws8NIL5kynSdRCA7d6RX7gfX4irRMAgMFyLDDwJLW',
-    };
+function replyMessage(uid, options) {
     axios(options)
         .then(function (response) {
             // handle success
