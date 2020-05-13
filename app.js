d016a12065cf6ab8071957bbf27a9b3b7a758c2b
Chung Biện <chungch251997@gmail.com>
2020-05-13 10:46:06 +0700

    Xoa cros

--- app.js
+++ app.js
@@ -13,29 +13,7 @@ var app = express();
 var cors = require('cors');
 app.use(cors());
 // Add headers
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://chungbt.vbee.vn/');
 
-    // Request methods you wish to allow
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-    );
-
-    // Request headers you wish to allow
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-    );
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // view engine setup
 mongoose.Promise = global.Promise;
 app.use(logger('dev'));
