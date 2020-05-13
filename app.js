30b63e6422be9927b43b281ca7e94e273fe73b4a
Chung Biện <chungch251997@gmail.com>
2020-05-13 10:28:49 +0700

    them CORS endpoint client

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 // Add headers
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://chungbt.vbee.vn/');
 
     // Request methods you wish to allow
     res.setHeader(
