9c6104d1dc8bbb4ffdfca5dbe0ec04c2e84c15e1
Chung Biện <chungch251997@gmail.com>
2020-05-30 00:22:02 +0700

    Delete router test

--- controllers/ZaloController.js
+++ controllers/ZaloController.js
@@ -3,6 +3,9 @@ const botService = require('../services/bot');
 var { CLIENT_ENDPOINT } = require('../constants');
 
 const replyMessage = async (req, res) => {
+    console.log('====================================');
+    console.log(req.body);
+    console.log('====================================');
     let { recipient, sender, event_name, message } = req.body;
     let msgText = message !== undefined ? message.text : '';
     let msgId = message !== undefined ? message.msg_id : '';
